28645a4e30413360b9dbc99e329f844cd26649f1
Luis <luismhernandez@gmail.com>
2022-04-21 22:24:46 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+
+
+
+
+function writeCards(names, evento) {
+    const newNames = []
+    for (let i = 0; i < names.length; i++) {
+        let message = (`Thank you, ${names[i]}, for the wonderful ${evento} gift!`);
+        newNames.push(message);
+        
+  }
+  console.log(newNames)
+  return newNames;
+}
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const evento = ["surprise"];
+
+writeCards(names,evento);
+
+
+
+function countDown(numero) {
+    //let counter = 0
+    while (numero >= 0) {
+        console.log(numero);
+        numero--;
+
+    }
+}
+
+countDown(10);
